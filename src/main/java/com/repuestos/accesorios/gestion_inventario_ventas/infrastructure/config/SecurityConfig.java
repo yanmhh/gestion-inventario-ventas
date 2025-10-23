bc5ec568cfec675544ab6626c07d677087094c75
Marcos-Huaman <marcoshuaman3009@gmail.com>
2025-10-23 12:39:27 -0500

    feat(deploy): Corregir errores de dependencia en SecurityConfig.

--- src/main/java/com/repuestos/accesorios/gestion_inventario_ventas/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/repuestos/accesorios/gestion_inventario_ventas/infrastructure/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(allowedOrigin));
+        configuration.setAllowedOrigins(List.of(allowedOrigin.split(",")));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
